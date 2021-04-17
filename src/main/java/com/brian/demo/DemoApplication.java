191c357964529cc5ab6a885a1bce9b649160050e
1092249319@qq.com <1092249319@qq.com>
2021-04-17 11:53:04 +0800

    optimize

--- src/main/java/com/brian/demo/DemoApplication.java
+++ src/main/java/com/brian/demo/DemoApplication.java
@@ -1,11 +1,5 @@
 package com.brian.demo;
 
-import java.util.Properties;
-
-import org.apache.kafka.clients.producer.KafkaProducer;
-import org.apache.kafka.clients.producer.ProducerConfig;
-import org.apache.kafka.clients.producer.ProducerRecord;
-import org.apache.kafka.common.serialization.StringSerializer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
