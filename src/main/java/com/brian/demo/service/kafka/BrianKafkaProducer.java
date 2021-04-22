b775d4dc9e29d913beae9fc8dd6f62c78311ce19
1092249319@qq.com <1092249319@qq.com>
2021-04-22 17:04:05 +0800

    add mode for kafka producer

--- src/main/java/com/brian/demo/service/kafka/BrianKafkaProducer.java
+++ src/main/java/com/brian/demo/service/kafka/BrianKafkaProducer.java
@@ -21,7 +21,7 @@ public class BrianKafkaProducer {
 
     static {
         props = new Properties();
-        props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "49.233.143.198:9092");
+        props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
         props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
         props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
     }
