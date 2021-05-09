93871af44f32e2d258391d430ffa91046e6f5a33
1092249319@qq.com <1092249319@qq.com>
2021-05-09 09:39:30 +0800

    upate the folder structure

--- src/main/java/com/brian/BrianApplication.java
+++ src/main/java/com/brian/BrianApplication.java
@@ -1,13 +1,13 @@
-package com.brian.demo;
+package com.brian;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoApplication {
+public class BrianApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(DemoApplication.class, args);
+        SpringApplication.run(BrianApplication.class, args);
 
     }
 }
