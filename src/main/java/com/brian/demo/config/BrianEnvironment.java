e483d16d9b9991df76574065c4847c03c4be1ffa
1092249319@qq.com <1092249319@qq.com>
2021-05-09 08:58:07 +0800

    get application propertirs by ConfigurableEnvironment

--- src/main/java/com/brian/demo/config/BrianEnvironment.java
+++ src/main/java/com/brian/demo/config/BrianEnvironment.java
@@ -1,92 +1,31 @@
 package com.brian.demo.config;
 
-import com.brian.demo.controller.CheckController;
-import org.springframework.beans.BeansException;
-import org.springframework.beans.factory.BeanClassLoaderAware;
-import org.springframework.beans.factory.BeanFactory;
-import org.springframework.beans.factory.BeanFactoryAware;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
-import org.springframework.context.ResourceLoaderAware;
-import org.springframework.core.NestedExceptionUtils;
+import org.springframework.boot.env.OriginTrackedMapPropertySource;
 import org.springframework.core.env.ConfigurableEnvironment;
 import org.springframework.core.env.MutablePropertySources;
-import org.springframework.core.io.Resource;
-import org.springframework.core.io.ResourceLoader;
-import org.springframework.core.io.support.PropertiesLoaderUtils;
 import org.springframework.stereotype.Component;
-import org.springframework.util.ClassUtils;
 
-import java.util.Map;
-import java.util.Properties;
+import lombok.extern.slf4j.Slf4j;
 
+import java.util.stream.Stream;
+
+@Slf4j
 @Component
-public class BrianEnvironment implements CommandLineRunner, ResourceLoaderAware,
-        BeanFactoryAware, BeanClassLoaderAware {
+public class BrianEnvironment implements CommandLineRunner {
 
     @Autowired
     private ConfigurableEnvironment environment;
 
-    @Autowired
-    private ConfigurableEnvironment configurableEnvironment;
-
-    private ResourceLoader resourceLoader;
-
-    private BeanFactory beanFactory;
-
-    private ClassLoader classLoader;
-
     @Override
     public void run(String... args) throws Exception {
-        System.out.println(environment);
-        MutablePropertySources propertySources1 = environment.getPropertySources();
-        String property = environment.getProperty("project.version");
-        Resource resource = resourceLoader.getResource("file:pom.xml");
-        System.out.println(resource);
-        try {
-            Properties properties = PropertiesLoaderUtils.loadProperties(resource);
-        } catch (Exception e) {
-            Throwable mostSpecificCause = NestedExceptionUtils.getMostSpecificCause(e);
-            System.out.println(mostSpecificCause.getMessage());
-            Throwable rootCause = NestedExceptionUtils.getRootCause(e);
-            System.out.println(rootCause.getMessage());
-        }
+        String serviceVersion = environment.getProperty("project.version");
+        log.info("===== service version: {} =====", serviceVersion);
 
-        CheckController bean = this.beanFactory.getBean(CheckController.class);
-        System.out.println(bean);
-
-        MutablePropertySources propertySources = configurableEnvironment.getPropertySources();
-
-        Map<String, Object> systemProperties = configurableEnvironment.getSystemProperties();
-
-        Map<String, Object> systemEnvironment = configurableEnvironment.getSystemEnvironment();
-
-        String[] defaultProfiles = configurableEnvironment.getDefaultProfiles();
-
-
-    }
-
-    @Override
-    public void setResourceLoader(ResourceLoader resourceLoader) {
-        this.resourceLoader = resourceLoader;
-    }
-
-    @Override
-    public void setBeanFactory(BeanFactory beanFactory) throws BeansException {
-        this.beanFactory = beanFactory;
-    }
-
-    @Override
-    public void setBeanClassLoader(ClassLoader classLoader) {
-        this.classLoader = classLoader;
-    }
-
-
-    private ClassLoader getClassLoader() {
-        if (this.resourceLoader != null) {
-            return this.resourceLoader.getClassLoader();
-        }
-        return ClassUtils.getDefaultClassLoader();
+        MutablePropertySources propertySources = environment.getPropertySources();
+        OriginTrackedMapPropertySource propertySource =  (OriginTrackedMapPropertySource) propertySources.get("applicationConfig: [classpath:/application.properties]");
+        Stream.of(propertySource.getPropertyNames()).forEach(name -> log.info(">>>>> application property name:{}", name)); 
     }
 
 
