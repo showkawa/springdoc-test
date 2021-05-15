d75459cdc55ca8aae2555f1e919fbb5e328add2c
1092249319@qq.com <1092249319@qq.com>
2021-05-15 22:00:15 +0800

    add the query kafka topic api

--- src/main/java/com/brian/demo/controller/KafkaInfoController.java
+++ src/main/java/com/brian/demo/controller/KafkaInfoController.java
@@ -1,85 +1,11 @@
 package com.brian.demo.controller;
 
-import java.util.Arrays;
-import java.util.Properties;
-import java.util.concurrent.ExecutionException;
 
-import org.apache.kafka.clients.admin.AdminClient;
-import org.apache.kafka.clients.admin.AdminClientConfig;
-import org.apache.kafka.clients.admin.DescribeClusterResult;
-import org.apache.kafka.clients.admin.DescribeConfigsResult;
-import org.apache.kafka.clients.admin.DescribeConsumerGroupsResult;
-import org.apache.kafka.clients.admin.DescribeLogDirsResult;
-import org.apache.kafka.clients.admin.DescribeReplicaLogDirsResult;
-import org.apache.kafka.clients.admin.DescribeTopicsResult;
-import org.apache.kafka.clients.admin.KafkaAdminClient;
-import org.apache.kafka.clients.admin.ListConsumerGroupsResult;
-import org.apache.kafka.clients.admin.ListTopicsResult;
-import org.apache.kafka.common.TopicPartitionReplica;
-import org.apache.kafka.common.config.ConfigResource;
-import org.apache.kafka.common.config.ConfigResource.Type;
 import org.springframework.web.bind.annotation.RestController;
 
-import lombok.extern.slf4j.Slf4j;
 
 @RestController
-@Slf4j
 public class KafkaInfoController {
 
-    private AdminClient adminClient;
-
-    public KafkaInfoController() throws InterruptedException, ExecutionException{
-        Properties props = new Properties();
-        props.put(AdminClientConfig.BOOTSTRAP_SERVERS_CONFIG,"49.233.143.198:9092");
-        adminClient = KafkaAdminClient.create(props);
-        ListTopicsResult tr = listTopic();
-        log.info("=== topics ===: {} ", tr.names().get());
-        log.info("=== listings ===: {} ", tr.listings().get());
-
-
-        ListConsumerGroupsResult lcg = listConsumerGroups();
-        log.info("=== consumerGroupListing ===: {} ", lcg.all().get());
-
-        // DescribeAclsResult aclResult = adminClient.describeAcls(null);
-
-        DescribeClusterResult clusterResult = adminClient.describeCluster();
-        log.info("=== describeAclsResult ===: {} ", clusterResult.nodes().get());
-
-
-        DescribeConfigsResult configResult = adminClient.describeConfigs(Arrays.asList(new ConfigResource(Type.TOPIC, "brian_t")));
-        log.info("=== describeConfigsResult ===: {} ", configResult.all().get());
-
-
-        DescribeConsumerGroupsResult consumerGroupsResult = adminClient.describeConsumerGroups(Arrays.asList("console-consumer-43625"));
-        log.info("=== describeConsumerGroupsResult ===: {} ", consumerGroupsResult.all().get());
-
-
-        /**
-         * Delegation Token requests are not allowed on PLAINTEXT/1-way SSL channels 
-         *      and on delegation token authenticated channels.
-         */
-        // DescribeDelegationTokenResult delegationTokenResult = adminClient.describeDelegationToken();
-        // log.info("=== describeDelegationTokenResult ===: {} ", delegationTokenResult.delegationTokens().get());
-
-        DescribeLogDirsResult logDirsResult = adminClient.describeLogDirs(Arrays.asList(0));
-        log.info("=== describeLogDirsResult ===: {} ", logDirsResult.all().get());
-
-
-        DescribeReplicaLogDirsResult describeReplicaLogDirsResult = 
-                adminClient.describeReplicaLogDirs(Arrays.asList(new TopicPartitionReplica("brian_t", 0, 0)));
-                log.info("=== describeReplicaLogDirs ===: {} ", describeReplicaLogDirsResult.all().get());
-        
-        
-        DescribeTopicsResult describeTopics = adminClient.describeTopics(Arrays.asList("brian_t"));
-        log.info("=== describeTopics ===: {} ", describeTopics.all().get());
-        
-    }
-
-    public ListTopicsResult listTopic() {
-       return adminClient.listTopics();
-    }
-
-    public ListConsumerGroupsResult listConsumerGroups() {
-        return adminClient.listConsumerGroups();
-     }
+   
 }
---
+++ src/main/java/com/brian/demo/service/kafka/BrianKafkaAdminClientService.java
@@ -0,0 +1,80 @@
+package com.brian.demo.service.kafka;
+
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.List;
+import java.util.Map;
+import java.util.Properties;
+import java.util.concurrent.ExecutionException;
+
+import org.apache.kafka.clients.admin.AdminClient;
+import org.apache.kafka.clients.admin.AdminClientConfig;
+import org.apache.kafka.clients.admin.Config;
+import org.apache.kafka.clients.admin.ConsumerGroupDescription;
+import org.apache.kafka.clients.admin.ConsumerGroupListing;
+import org.apache.kafka.clients.admin.KafkaAdminClient;
+import org.apache.kafka.clients.admin.TopicDescription;
+import org.apache.kafka.clients.admin.TopicListing;
+import org.apache.kafka.clients.admin.DescribeReplicaLogDirsResult.ReplicaLogDirInfo;
+import org.apache.kafka.common.Node;
+import org.apache.kafka.common.TopicPartitionReplica;
+import org.apache.kafka.common.config.ConfigResource;
+import org.apache.kafka.common.config.ConfigResource.Type;
+import org.apache.kafka.common.requests.DescribeLogDirsResponse.LogDirInfo;
+import org.springframework.stereotype.Component;
+
+
+@Component
+public class BrianKafkaAdminClientService {
+    
+    private AdminClient adminClient;
+
+    public BrianKafkaAdminClientService() {
+        Properties props = new Properties();
+        props.put(AdminClientConfig.BOOTSTRAP_SERVERS_CONFIG,"localhost:9092");
+        adminClient = KafkaAdminClient.create(props);
+
+        // DescribeAclsResult aclResult = adminClient.describeAcls(null);
+
+        /**
+         * Delegation Token requests are not allowed on PLAINTEXT/1-way SSL channels 
+         *      and on delegation token authenticated channels.
+         */
+        // DescribeDelegationTokenResult delegationTokenResult = adminClient.describeDelegationToken();
+        // log.info("=== describeDelegationTokenResult ===: {} ", delegationTokenResult.delegationTokens().get());
+    }
+
+    public Collection<TopicListing> getTopics() throws InterruptedException, ExecutionException {
+       return adminClient.listTopics().listings().get();
+    }
+
+    public Collection<ConsumerGroupListing>  getConsumerGroups() throws InterruptedException, ExecutionException {
+        return adminClient.listConsumerGroups().all().get();
+    }
+
+
+    public Collection<Node>  getDescribeCluster() throws InterruptedException, ExecutionException {
+        return adminClient.describeCluster().nodes().get();
+    }   
+    
+    public Map<ConfigResource, Config>  getDescribeConfigs(String topicName) throws InterruptedException, ExecutionException {
+        return adminClient.describeConfigs(Arrays.asList(new ConfigResource(Type.TOPIC, topicName))).all().get();
+    }    
+    
+    public Map<String, ConsumerGroupDescription>  getDescribeConsumerGroups(String consumerGroupIds) throws InterruptedException, ExecutionException {
+        return adminClient.describeConsumerGroups(Arrays.asList(consumerGroupIds)).all().get();
+    }    
+    
+    public Map<Integer, Map<String, LogDirInfo>>  getDescribeLogDirs(List<Integer> brokers) throws InterruptedException, ExecutionException {
+        return adminClient.describeLogDirs(Arrays.asList(0)).all().get();
+    }    
+    
+    public Map<TopicPartitionReplica, ReplicaLogDirInfo>  getDescribeReplicaLogDirs(List<TopicPartitionReplica> replicas) throws InterruptedException, ExecutionException  {
+        return adminClient.describeReplicaLogDirs(replicas).all().get();
+    }    
+    
+    public Map<String, TopicDescription>  getDescribeTopics(List<String> topics) throws InterruptedException, ExecutionException {
+        return adminClient.describeTopics(topics).all().get();
+    }    
+
+}
