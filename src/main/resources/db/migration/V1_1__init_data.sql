2b17003814ffc6ba36394b84efe4889789c8a6f5
1092249319@qq.com <1092249319@qq.com>
2021-06-03 06:46:53 +0800

    add  flyway

---
+++ src/main/resources/db/migration/V1_1__init_data.sql
@@ -0,0 +1,22 @@
+INSERT into t_group values ('2c2de8dc-77dd-4384-84b2-674ace82a192','AAA','{"group_name":"AAA"}');
+INSERT into t_group values ('58212510-6d9d-4ee5-89b0-0fbb0059e8a2','ABB','{"group_name":"ABB"}');
+INSERT into t_group values ('3aa37450-a2b5-4480-ba24-314468bb34e9','ABC','{"group_name":"ABC"}');
+
+INSERT into t_entry values ('2c141e2e-bee5-4195-84fd-193a0cd1ba7f','AAA','aaa123','{}');
+INSERT into t_entry values ('a64e6ff5-840b-4d86-81eb-ed555d485b8d','AAA','aaaead','{}');
+INSERT into t_entry values ('3a2e6782-35e6-486a-bd0e-a03734e86f9b','AAA','dasd','{}');
+INSERT into t_entry values ('cf28b30a-cdba-4c2f-8812-2d6c45a1e974','AAA','edeee','{}');
+INSERT into t_entry values ('3e66c4d4-4e88-4757-a750-bc226df8df26','AAA','13123','{}');
+INSERT into t_entry values ('06cbbcb0-bd36-43d6-9c76-b47b6d4f64e2','AAA','qweqwe','{}');
+INSERT into t_entry values ('4e88a9e6-85be-4774-98af-758a170b185b','ABB','qweqweqwe','{}');
+INSERT into t_entry values ('3c277bfe-ea10-4875-9791-3c99717e8bd3','ABB','12312','{}');
+INSERT into t_entry values ('05e098e4-1750-4192-8583-f2f34f237cef','ABB','8765','{}');
+INSERT into t_entry values ('b9271a16-06ce-41d1-bbd3-f3785df6faa0','ABB','edc4rfv','{}');
+INSERT into t_entry values ('11ed92f8-1a21-4c2b-979e-994f4083af5b','ABB','5tgb6yhn','{}');
+INSERT into t_entry values ('a7777cf9-3390-470b-9b33-089d5c92f507','ABC','1qaz2wsx','{}');
+INSERT into t_entry values ('6dae44d0-44ed-45d2-a0ae-dd5f89177fbc','ABC','5tg6yhn','{}');
+INSERT into t_entry values ('b31ec4d0-bd4d-44e8-bd99-9459a153a4bd','ABC','7ujm8ik','{}');
+INSERT into t_entry values ('f0aa3252-46fb-4ae0-a08e-2bf80a705a73','ABC','werwerwe','{}');
+INSERT into t_entry values ('e3073172-1984-4d65-82fb-11c5886edb05','ABC','erte454','{}');
+INSERT into t_entry values ('ffb23857-fd80-4ab7-a922-cd6af60bb25b','ABC','676asdasee','{}');
+INSERT into t_entry values ('8c3d87c4-007f-408a-9800-a22f019de5de','ABC','edcrrtg','{}');
